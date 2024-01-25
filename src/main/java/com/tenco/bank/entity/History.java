7ef959908001765e09d786ed4f7c6573674803e0
KBJ97 <sjn04037@naver.com>
2024-01-25 16:05:14 +0900

    모델링 처리까지

---
+++ src/main/java/com/tenco/bank/entity/History.java
@@ -0,0 +1,23 @@
+package com.tenco.bank.entity;
+
+import java.security.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class History {
+	
+	private Integer id;
+	private Long amount;
+	private Long wBalance;
+	private Long dBalance;
+	private Integer wAccountId;
+	private Integer dAccountId;
+	private Timestamp createdAt;
+}
